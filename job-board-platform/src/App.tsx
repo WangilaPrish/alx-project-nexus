ac34e23adfad76101186630a657efb7f0d10e34c
WangilaPrish <nabwonyaprish@gmail.com>
2025-08-08 11:38:29 +0300

    refactor: remove ExternalJobsExplorer and JobsDashboard routes from App component

--- job-board-platform/src/App.tsx
+++ job-board-platform/src/App.tsx
@@ -1,7 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '../src/pages/Home';
-import ExternalJobsExplorer from './components/ExternalJobsExplorer';
-import JobsDashboard from './components/JobsDashboard';
 import AppliedJobsPage from './pages/AppliedJobsPage';
 import JobsPage from './pages/JobsPage';
 import LoginPage from './pages/LoginPage';
@@ -14,8 +12,6 @@ const App = () => {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/jobs" element={<JobsPage />} />
-            <Route path="/all-jobs" element={<JobsDashboard />} />
-            <Route path="/external-jobs" element={<ExternalJobsExplorer />} />
             <Route path="/applied" element={<AppliedJobsPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
